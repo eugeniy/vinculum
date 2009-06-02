f184d47afa81b65f1863f0dd45c36bc314c8fd5a
eugeniy <burump@gmail.com>
2009-06-02 14:10:17 -0700

    Render, factory, getters and setters.
    Major parts are somewhat tested.

--- test.php
+++ test.php
@@ -1,12 +1,130 @@
 <?php
 
+/**
+ * A few bloated tests, these should be reorganized and updated.
+ */
+
 require_once 'view.php';
 
 class ViewTest extends UnitTestCase {
-
-	function testConstructorFileNameSetting()
+	
+	
+	private $TestTemplateFileName = 'test_template_file.txt';
+	
+	
+	function setUp()
 	{
-		//$this->assertFalse(false);
+		// View class requires a template file, so create an empty file.
+		@touch($this->TestTemplateFileName);
 	}
+
+	function tearDown()
+	{
+		@unlink($this->TestTemplateFileName);
+	}
+	
+	
+	function testCreateTestFile()
+	{
+		$this->assertTrue(is_readable($this->TestTemplateFileName));
+		
+		@unlink($this->TestTemplateFileName);
+		$this->assertFalse(file_exists($this->TestTemplateFileName));
+	}
+	
+	
+	function testConstructorSetFileName()
+	{
+		$view = new View($this->TestTemplateFileName);
+		$this->assertTrue($view->GetFileName() == $this->TestTemplateFileName);
+	}
+	
+
+	function testDataArrayGetSet()
+	{
+		$view = new View();
+		$this->assertTrue(is_array($view->GetDataArray()));
+		
+		$view->SetDataArray("this shouldn't work");
+		$this->assertTrue(is_array($view->GetDataArray()));
+		
+		$view->SetDataArray(array('test'=>'variable'));
+		$this->assertTrue($view->GetDataArray() == array('test'=>'variable'));
+		$this->assertTrue($view->test == 'variable');
+	}
+	
+
+	function testVariableSetAndGet()
+	{
+		$view = new View($this->TestTemplateFileName);
+		
+		$view->SetVariable('testMessage', 'Hello, World!');
+		$this->assertTrue($view->GetVariable('testMessage') == 'Hello, World!');
+		
+		// Test existing variables
+		$this->assertFalse($view->GetVariable('fileName') == $this->TestTemplateFileName);
+		$this->assertFalse($view->fileName == $this->TestTemplateFileName);
+		$this->assertFalse(isset($view->fileName));
+		$view->fileName = 'test';
+		$this->assertTrue($view->fileName == 'test');
+		$this->assertTrue($view->GetFileName() == $this->TestTemplateFileName);
+		
+		$view->SetVariable('testNumeric', 123);
+		$this->assertTrue($view->GetVariable('testNumeric') === 123);
+		
+		// Alternative syntax with magic methods
+		$this->assertTrue($view->testNumeric === 123);
+		$view->SomeTestVariable = 'FooBar';
+		$this->assertTrue($view->GetVariable('SomeTestVariable') == 'FooBar');
+		$this->assertTrue($view->SomeTestVariable == 'FooBar');
+		
+		$view->SetVariable(456, 'Goats eat grass');
+		$this->assertTrue($view->GetVariable(456) == 'Goats eat grass');
+		
+		$view->SetVariable(null, 'Null variable');
+		$this->assertTrue($view->GetVariable(null) == 'Null variable');
+		$this->assertTrue($view->GetVariable('') == 'Null variable');
+		$this->assertFalse($view->GetVariable(0) == 'Null variable');
+
+		$anotherView = new View($this->TestTemplateFileName);
+		
+		$view->SetVariable('testObject', $anotherView);
+		$this->assertTrue(is_object($view->GetVariable('testObject')));
+	}
+	
+	function testRenderTemplate()
+	{
+		$view = new View($this->TestTemplateFileName);
+		$view->TestVariable = 'bar';
+		
+		$this->assertTrue($view->Render() == '');
+		
+		file_put_contents($this->TestTemplateFileName, 'Some template data');
+		$this->assertTrue($view->Render() == 'Some template data');
+		
+		file_put_contents($this->TestTemplateFileName, 'Foo <?php echo $this->FakeVar; ?> data');
+		$this->assertTrue($view->Render() == 'Foo  data');
+		
+		file_put_contents($this->TestTemplateFileName, 'Foo <?php echo $TestVariable; ?> data');
+		$this->assertTrue($view->Render() == 'Foo bar data');
+		
+		file_put_contents($this->TestTemplateFileName, 'Foo <?php echo $this->TestVariable; ?> data');
+		$this->assertTrue($view->Render() == 'Foo bar data');
+		
+		// Alternative syntax with magic method
+		$this->assertTrue($view == 'Foo bar data');
+		
+		
+		@unlink($this->TestTemplateFileName);
+		$this->assertTrue($view == '');
+		
+		$view->SetFileName(null);
+		$this->assertTrue($view == '');
+		
+		$view->SetFileName('');
+		$this->assertTrue($view == '');
+		
+	}
+	
 
 }
--- view.php
+++ view.php
@@ -1,39 +1,81 @@
 <?php
 /**
- * 
  * A simple view class for the MVC pattern.
  * 
  * @author Eugeniy Kalinin
+ * @license http://pandabytes.info/license/
+ * @link http://pandabytes.info/vinculum/
  *
  */
 class View {
 	
-	private $fileName;
-	private $data;
+	protected $fileName;
+	protected $data = array();
 
 	/**
 	 * Constructor.
-	 * @param $fileName
-	 * @param $data
-	 * @return unknown_type
+	 * @param String $fileName
+	 * @param Array $data
+	 * @return void
 	 */
-	function __construct($fileName, $data = null)
+	public function __construct($fileName = null, $data = null)
 	{
-		if (is_string($fileName) AND is_readable($fileName))
-		{
+		$this->SetFileName($fileName);
+		$this->SetDataArray($data);
+	}
+
+	
+	/**
+	 * Factory method.
+	 * Creates and returns an instance of the class.
+	 * @example echo View::Factory("checkout.php");
+	 * @param String $fileName
+	 * @param Array $data
+	 * @return Class instance
+	 */
+	public static function Factory($fileName = null, $data = null)
+	{
+		return new View($fileName, $data);
+	}
+	
+	
+	/**
+	 * Outputs the template onto the screen.
+	 * Variables set up to this point are passed to the template.
+	 * @return string template data
+	 */
+	public function Render($fileName = null)
+	{
+		// Allow dynamic file name changes
+		if ($fileName !== null)
 			$this->SetFileName($fileName);
+		
+		// Start capturing the output
+		ob_start();
+		
+		if (is_string($this->fileName) AND is_readable($this->fileName))
+		{
+			// Import variables into the namespace
+			extract($this->data, EXTR_SKIP);
+			// Include the view, allow access to class instance
+			include $this->fileName;
 		}
+		// Dump the buffer and return the output
+		return ob_get_clean();
 	}
 	
 
 	/**
 	 * File name setter.
 	 * @param string $fileName
+	 * @return Current class instance
 	 */
 	public function SetFileName($fileName)
 	{
 		$this->fileName = $fileName;
+		return $this;
 	}
+	
 	
 	/**
 	 * File name getter.
@@ -43,24 +85,86 @@ class View {
 	{
 		return $this->fileName;
 	}
-	
-	
-	
-	
-/*	public function Render()
+
+
+	/**
+	 * Set the variable for use in the template.
+	 * @param string $key - variable name
+	 * @param $value - variable value
+	 * @return Current class instance
+	 */
+	public function SetVariable($key, $value)
 	{
-		// Start capturing the output
-		ob_start();
-		
-		if (isset($this->_file))
-		{
-			// Import variables into the namespace
-			extract($this->_data, EXTR_SKIP);
-			// Include the view, allow access to class instance
-			include $this->_file;
-		}
-		// Dump the buffer and return the output
-		return ob_get_clean();
-	}*/
+		$this->data[$key] = $value;
+		return $this;
+	}
+	
+	
+	/**
+	 * Get the variable to be used in the template.
+	 * @param string $key - variable name
+	 * @return Value stored under the given variable name or NULL if not found.
+	 */
+	public function GetVariable($key)
+	{
+		if (isset($this->data[$key]))
+			return $this->data[$key];
+		return null;
+	}
+	
+	
+	/**
+	 * Sets variables using an associative array.
+	 * @param Array $data
+	 * @return Current class instance
+	 */
+	public function SetDataArray($data)
+	{
+		if (is_array($data))
+			$this->data = array_merge($this->data, $data);
+		return $this;
+	}
+
+	
+	/**
+	 * Data array getter.
+	 * @return Array
+	 */
+	public function GetDataArray()
+	{
+		return $this->data;
+	}
+	
+	
+	/**
+	 * Magic method to set unknown properties.
+	 * It can be used to pass variables to the template.
+	 * @example $view->Message = "Hello, World!";
+	 */
+	public function __set($key, $value)
+	{
+		$this->SetVariable($key, $value);
+	}
+
+	
+	/**
+	 * Magic method to get unknown properties.
+	 * It can be used to retrieve template variables.
+	 * @example echo $view->Message;
+	 */
+	public function __get($key)
+	{
+		return $this->GetVariable($key);
+	}
+	
+	
+	/**
+	 * Magic method to automatically render when instance is used as a string.
+	 * @example echo $view;
+	 */
+	public function __toString()
+	{
+		return $this->Render();
+	}
 	
 }
